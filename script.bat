18c45f68d69d351e7f0334d43f0755f7e9df8bb8
Alessandro Azevedo <aleazevedo84@outlook.com>
2023-05-15 20:17:29 -0300

    Adicionado arquivos para deploy da aplicação

---
+++ script.bat
@@ -0,0 +1,18 @@
+echo "Criando as imagens:"
+
+docker build -t alezevedo84/projeto-backend:1.0 backend/.
+docker build -t alezevedo84/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens:"
+
+docker push -t alezevedo84/projeto-backend:1.0 
+docker push -t alezevedo84/projeto-database:1.0 
+
+echo "Criado servio no cluster kubernetes:"
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments:"
+kubectl apply -f ./deployment.yml
+
+
